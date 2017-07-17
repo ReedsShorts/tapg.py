72376d5b5b5a4b8cd1e28e16266c972c452b4e3f
ReedsShorts <kidsshort@gmail.com>
2017-07-17 14:41:12 -0700

    Update tapgcode.py

--- tapgcode.py
+++ tapgcode.py
@@ -2,4 +2,6 @@ c = 0
 from random import randint
 print("Thank you for playing tapg. Please enter your password from save. Type in 000000 to start a new game. /n")
 def game():
-  c = input("Would you like to hear the controls? (Y/N)")
+    c = input("Would you like to hear the controls? (Y/N)")
+    if c = "Y":
+        print("Type u to move up, d to move down, l to move left and r to move right. /n 
