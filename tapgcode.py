a8edbe136cbafd7eac2dd34a3ebe718fb6eb0e00
ReedsShorts <kidsshort@gmail.com>
2017-07-18 13:07:08 -0700

    Update tapgcode.py

--- tapgcode.py
+++ tapgcode.py
@@ -4,7 +4,7 @@ print("Thank you for playing tapg. Please enter your password from save. Type in
 def game():
     c = input("Would you like to hear the controls? (Y/N)")
     if c = "Y":
-        print("Type u to move up, d to move down, l to move left ,and r to move right. /n Type f to fight, m to use magic, h to heal, and s to save  
+        print("Type u to move up, d to move down, l to move left ,and r to move right. /n Type f to fight, m to use magic, h to heal, and s to save") 
     area = 0
     while 1:
         m = input()
@@ -14,3 +14,8 @@ def game():
             area = area + 2
         elif m = "d":
             area = area + 3
+        elif m = "r":
+              area = area + 4
+    while 1:
+        if (area = 0 and (m = "d" or m = "r")) or (area = 62 and (m = "l" or m = "d")) or (area = 31 and (m = "u" or m = :
+              print("Invalid move") 
